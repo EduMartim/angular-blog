25bb8e77a49ace6a74aff8682d4d1516b753f304
edumartim <edumartim@gmail.com>
2023-06-01 22:04:16 -0300

    Finilizing coding, structures, new features, etc

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Lorem ipsum dolor sit amet.",
+    "description": "Lorem ipsum dolor, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    "photoCover": "https://th.bing.com/th/id/R.b198b72af3b2a07673308985bd7919dd?rik=KUIziS0uCAOXqw&pid=ImgRaw&r=0",
+
+  },
+
+  {
+    "id" : "2",
+    "title": "Lorem ipsum dolor, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    "description": "Lorem ipsum dolor sit amet.",
+    "photoCover": "https://miro.medium.com/v2/resize:fit:720/format:webp/1*GF2-oJG_TschrTYEoCudkg.png",
+
+  },
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,8 +9,5 @@ export class HomeComponent implements OnInit {
 
   constructor () { }
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
+  ngOnInit(): void { }
 }
